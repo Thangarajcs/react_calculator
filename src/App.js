43174208f067e5a6b51245b3a85efbb1ca797231
Thangaraj <thangaraj.mayilsamy@zinier.com>
2021-02-07 06:48:41 +0530

    added goal-1 goal2 goal 3

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css';
+import {CalculatorUI} from "./Components/calculator";
+
+function App() {
+  return (
+    <div className="App">
+     <CalculatorUI/>
+    </div>
+  );
+}
+
+export default App;
